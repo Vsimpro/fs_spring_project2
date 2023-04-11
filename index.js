a5e1e64e6788cfa303cafa861f811705e917b22f
Vsimpro <jsimrpo@gmail.com>
2023-04-12 01:52:58 +0300

    mostly planning

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 // Imports
 require("dotenv").config();
+const express = require("express");
+
+/* Global Variables */
+const app = express();
+      app.use(express.json());
+      app.use(express.static(__dirname + "/templates/static"))
+
+let HOST = process.env.HOST
+let PORT = process.env.PORT
+
+app.listen(PORT, function() { 
+    console.log("[+] [Index] Server starting on http://" + HOST + ":" + PORT) 
+});
+
+// TODO: The App itself. Use single page design.
+app.get("/", function(request, response) {
+    console.log("[>] [Index] GET '/'");
+    response.send("200"); 
+    return 0; 
+});
+
+app.get("/*", function(request, response) {
+    console.log("[>] [Index] GET '/?'");
+    response.send("404"); 
+    return 0; 
+});
+
+
+// TODO: 
+// Get Auth: (maybe use JWT?)
+// /api/login  : create access key. POST
+
+// /api/logout : delete access key. GET (?)
+
+// No auth:
+// /api/getall : return all the comments. POST
+
+// /api/:id : return all the comments made by one user POST
+
+
+// Needs auth:
+// /api/add : add a new comment if auth key allows it. POST
+
+// /api/update/:id : update earlier made comments. POST
+
+// /api/delete/:id delete a comment made earlier. POST
--- modules/mongo.js
+++ modules/mongo.js
@@ -1,4 +1,9 @@
+// Imports
 require("dotenv").config();
+const Mongoose = require("mongoose");
+
+// Might not end up needing this.
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 class Connection {
     constructor() {
@@ -6,7 +11,29 @@ class Connection {
 
         this.db_password = process.env.MONGO_PASSWD;
         this.db_username = process.env.MONGO_USERNAME;
+
+        this.uri = "mongodb+srv://" + this.db_username + ":" + this.db_password +  "@fullstackcluster.awpvpm2.mongodb.net/";
         
+        try {
+            this.connect()
+        } catch(error) {
+            console.log("[!] [Mongo] Could not connect to the Database while initialising.")
+        }
+    }
+
+    connect() {
+        try {
+            Mongoose.connect( this.uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+
+            console.log("[+] [Mongo] Connection made. ")
+
+        } catch (error) {
+            // TODO: Find out why.
+            console.log("[!] [Mongo] Can't connect to MongoDB.")
+        }
     }
 
     // TODO: Get desired data
